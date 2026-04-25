d3482fcb01a8f5280c8984a4adeffc5e6b4fc50e
amuif <amudihayat@gmail.com>
2026-04-25 11:02:40 +0300

    feat:nav is done

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import Hero from "./components/hero";
 import NavBar from "./components/nav-bar";
 function App() {
   return (
-    <div className="font-sans">
-      <NavBar />
+    <div className="font-sans relative">
+      <div className="absolute top-0 left-0 w-full z-50">
+        <NavBar />
+      </div>
       <Hero />
     </div>
   );
